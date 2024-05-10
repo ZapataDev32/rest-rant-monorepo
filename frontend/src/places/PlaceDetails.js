8faa3fa0d7eb8b7c666588fd15f1f6e3829e004c
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-05-09 21:58:14 -0700

    project all setup

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useHistory, useParams } from "react-router";
 import CommentCard from "./CommentCard";
 import NewCommentForm from "./NewCommentForm";
+import { CurrentUser } from "../contexts/CurrentUser";
 
 function PlaceDetails() {
   const { placeId } = useParams();
 
   const history = useHistory();
+
+  const { currentUser } = useContext(CurrentUser);
 
   const [place, setPlace] = useState(null);
 
@@ -107,6 +110,21 @@ function PlaceDetails() {
     });
   }
 
+  let placeActions = null;
+
+  if (currentUser?.role === "admin") {
+    placeActions = (
+      <>
+        <a className="btn btn-warning" onClick={editPlace}>
+          Edit
+        </a>
+        <button type="submit" className="btn btn-danger" onClick={deletePlace}>
+          Delete
+        </button>
+      </>
+    );
+  }
+
   return (
     <main>
       <div className="row">
@@ -128,17 +146,7 @@ function PlaceDetails() {
           </h3>
           <h4>Serving {place.cuisines}.</h4>
           <br />
-          <a className="btn btn-warning" onClick={editPlace}>
-            Edit
-          </a>
-          {` `}
-          <button
-            type="submit"
-            className="btn btn-danger"
-            onClick={deletePlace}
-          >
-            Delete
-          </button>
+          {placeActions}
         </div>
       </div>
       <hr />
